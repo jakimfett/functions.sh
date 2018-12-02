6d04a57646d923068d5b5cc4b253fd5a33b2855e
tessa <jakimfett@assorted.tech>
2018-12-01 16:38:32 -0800

    Several partial refactors, stashed.

--- f.sh
+++ f.sh
@@ -15,13 +15,196 @@
 #exit 1
 ###
 
+# --- # --- # --- # --- #
+# life looks a lot like code.
+# --- # --- # --- # --- # --- # --- # --- #
+# huh. that's odd.
+# --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- #
 
 
+
+# configuration values
+declare -A config['start']="$(date +%s)"
+
+
+
+
+# location for the base directory installation
+# realpath makes cross platform easier
+config['installTo']="$(realpath ~/)/functions.sh/"
+config['invokedFrom']="$(pwd)"
+config['self']="${config['invokedFrom']}${0:1:${#0}}"
+config['repo']="ssh://git@github.com/jakimfett/functions.sh.git"
+config['branch']="development"
+# done with configuration values
+
+# state variables
+declare -A stateVar
+stateVar['depsPresent']=0
+stateVar['emptyDir']=0
+stateVar['gitRemote']=0
+stateVar['isGit']=0
+stateVar['doInstall']=0
+# done with state variables
+
+# exit code definitions
+declare -A exits
+exits['done']=0
+exits['general']=1
+exits['dependency']=2
+# done with exit codes
+
+
+
+# prerequisites
+declare depList=('git' 'ssh')
+declare -A missingDep
+# --- # --- # --- # --- # --- # --- # --- # --- # --- # --- # --- #
+# mmmm...poetry.
+# --- # --- # --- # --- # --- # --- # --- #
+# something else that is a lot like life.
+# --- # --- # --- # --- #
+
+
+
+echo
+# iterate through the dependencies list
+# check that the system knows that they exist
+for singleDep in ${depList[@]}; do
+
+	echo "checking for ${singleDep}..."
+	# ask the system where the dependency lives
+	declare depLoc="$(which ${singleDep})"
+
+	# add an entry to the missing dependencies array
+	# if the dependency location query to the system returns empty
+	if [ ! "${depLoc}" ];then
+		echo
+		echo "dependency '${singleDep}' not located..."
+		missingDep["${singleDep}"]=0
+	else
+		echo "found ${singleDep} at ${depLoc}"
+	fi
+
+done;echo
+
+
+
+# fail early, fail often
+if [ ! "${!missingDep[@]}" == '' ];then
+	echo
+	echo "error running script."
+	echo "missing: ${!missingDep[@]}"
+	echo "Please install dependencies to continue."
+	echo
+	echo "failmuffins"
+	exit ${exits['dependency']}
+else
+	echo "all dependencies found.";echo
+fi
+# done with prerequisites
+echo ${config['installTo']}
+
+# load functions.sh if it exists on the system
+if [ -d "${config['installTo']}" ];then
+
+	if [ ! "$(ls -A ~/functions.sh)" ];then
+		stateVar['emptyDir']=1
+	fi
+
+fi
+
+
+# clear
+echo "#=-              welcome to f.sh                  -=#"
+echo
+echo "#=- this script manages your local 'functions.sh' -=#"
+echo "#=-                                               -=#"
+echo "#=-                                               -=#"
+echo
+
+if [ ${stateVar['isGit']} == 1 ];then
+	echo "#=- a git repository exists at ~/functions.sh     -=#"
+	echo "#=- would you like to attempt to update this      -=#"
+	echo "#=- existing installation?                        -=#"
+
+	declare userInput
+	# solicite the user for some feedback
+	read "#=-                                       (y/n/?) -=#" userInput
+
+else
+
+	echo "#=- there is no current installation, install?    -=#"
+	echo "#=-                                               -=#"
+	echo "#=-                 (y/n/?)                       -=#"
+
+
+	declare rawInput
+	read -p "#=-                    " rawInput
+
+	case "${rawInput}" in
+		[yY])
+			stateVar['doInstall']=1
+			echo "#=-                                               -=#"
+
+			mkdir -p "${config['installTo']}"
+			cd "${config['installTo']}"
+
+			# check if remote repo is accessible
+
+			if [[ "$(git ls-remote --exit-code -h "${config['repo']}")" == *"${config['branch']}"* ]];then
+				if [ "$(pwd)" == "${config['installTo']}" ];then
+					echo git clone --single-branch -b "${config['branch']}" "${config['repo']}" .
+					echo "#=-                                               -=#"
+					echo "#=- initialized git repository...                 -=#"
+				fi
+			else
+				echo "#=-                                               -=#"
+				echo "#=- remote repository not found, cannot install   -=#"
+				echo "#=-                                               -=#"
+
+			fi
+		;;
+		[nN])
+			echo "#=-                                               -=#"
+			echo "#=- script will now exit                          -=#"
+			echo "#=-                                               -=#"
+		;;
+		*)
+			echo "#=-                                               -=#"
+			echo "input '${rawInput}' not recognized."
+			echo "#=-                                               -=#"
+		;;
+	esac
+	unset rawInput
+fi
+
+
+
+exit 13
+
+# mobility, finally!
+mkdir -p "${config['installTo']}"
+cd "${config['installTo']}"
+ls -lah
+
+
+echo "#=-                                          -=#"
+echo "#=-                                          -=#"
+echo "#=-                                          -=#"
+
+echo "#=-  -=#"
+echo "#=-  -=#"
+echo "#=-  -=#"
+echo "#=-  -=#"
+echo "#=-  -=#"
 echo "Hold on, here we go..."
 
 # @todo - set all variables via user-facing setup questions
 # Set the command character.
 commandChar='-'
+
+echo
 
 # Commands, short and long
 declare -A enabledCommands commandDescript
@@ -42,6 +225,44 @@ declare -a saneParam
 # Keep track of completeds
 declare -A doneList
 
+function cleanUp {
+	config['end']="$(date +%s)"
+	echo
+	echo "runtime: $(expr ${config['start']} - ${config['end']})"
+	echo
+	echo "Completed task(s):"
+	echo ${doneList[@]}
+	echo
+
+#	an attempt at exit code display
+#	encountered race conditions and variable nomenclature questions
+#	local code=$?
+#	echo "exit code ${?}"
+#	if [ "${exits['${?}']}" ]
+
+	echo "tmp folder contents:"
+	echo
+	ls -lah "./tmp"
+	echo
+	declare rawInput
+	read -p "Clear 'tmp' folder? (y/n/?): " rawInput
+	case "${rawInput}" in
+		[yY])
+			echo "Clearing tmp folder."
+			rm -rf ./tmp/*
+			echo "tmp folder cleared."
+		;;
+		[nN])
+			echo "Not clearing 'tmp' folder."
+		;;
+		*)
+			echo "input '${rawInput}' not recognized."
+		;;
+	esac
+	unset rawInput
+}
+
+#trap cleanUp EXIT INT QUIT TERM
 
 
 # sanitize and process short params
@@ -144,5 +365,4 @@ done
 # if no parameters have been passed, display the help
 if [ "${#saneParam}" == 0 ]; then
 	usageHelp
-	exit 0
 fi
