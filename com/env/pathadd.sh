056dafe4a0a97c6a701e2814f0be5442a98ec597
Tessa L. H. Lovelace <jakimfett@assorted.tech>
2019-04-29 17:47:22 -0700

    Diff before/after values for the user.

--- com/env/pathadd.sh
+++ com/env/pathadd.sh
@@ -5,4 +5,4 @@
 # Adds the working directory to your $PATH variable
 
 # @todo compare variables with diff bash
-echo "Old \$PATH contents:"; echo "${PATH}"; export oldPath=${PATH}; export PATH="${oldPath}:`pwd`"; echo "New path:"; echo "${PATH}"; echo; echo "Diff:"; diff `echo ${oldPath}` `echo $PATH`; echo; unset oldPath;
+echo "Old \$PATH contents:"; echo "${PATH}"; export oldPath=${PATH}; export PATH="${oldPath}:`pwd`"; echo "New path:"; echo "${PATH}"; echo; echo "Diff:"; diff <(echo ${oldPath}) <(echo $PATH); echo; unset oldPath;
