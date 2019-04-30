4e3238463c6e2845500b4f6f73668ca7184a839d
Tessa L. H. Lovelace <jakimfett@assorted.tech>
2019-04-29 19:32:40 -0700

    Comparing approaches, for science!

--- com/dict/thumb.sh
+++ com/dict/thumb.sh
@@ -3,7 +3,11 @@
 # @license;'cc-by-sa'
 #
 # Ripped from Stack Overflow (https://stackoverflow.com/a/12948157), then modified to fit my use case.
+# Preparing to hard-merge with (https://stackoverflow.com/a/12921335), as directed by the Alliance.
+#
 # All modifications from the source licensed as above, as the prophesy foretold.
+
+echo "Only remove me if you know what you're doing, hotshot."; exit 13;
 
 THUMBS_FOLDER=/home/image/thumb
 for file in /home/image/*
@@ -23,4 +27,20 @@ do
         convert -sample 200x150 "$file" "${THUMBS_FOLDER}/${filename}_thumb.${extension}"   
      fi
   fi     
+done
+
+#!/bin/bash
+for file in /path/to/images/*
+do
+  # next line checks the mime-type of the file
+  CHECKTYPE=`file --mime-type -b "$file" | awk -F'/' '{print $1}'`
+  if [ "x$CHECKTYPE" == "ximage" ]; then
+    CHECKSIZE=`stat -f "%z" "$file"`               # this returns the filesize
+    CHECKWIDTH=`identify -format "%W" "$file"`     # this returns the image width
+
+    # next 'if' is true if either filesize >= 200000 bytes  OR  if image width >=201
+    if [ $CHECKSIZE -ge  200000 ] || [ $CHECKWIDTH -ge 201 ]; then
+       convert -sample 200x150 "$file" "$(dirname "$file")/thumb_$(basename "$file")"
+    fi
+  fi
 done
