b4307653e285c1f4c39cf55a26e8c0fd2ed459ff
Tessa L. H. Lovelace <jakimfett@noreply.jakimfett.com>
2019-06-03 12:11:53 -0700

    Adding a way for remote systems to phone home.
    Need to come up with a DNS replacement, stat.

---
+++ com/net/bridge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# @author:'jakimfett'
+# @license: 'cc-by-sa'
+#
+# Constructed with bits from http://archive.is/xs4zQ
+
+bridgeFlag="-N -R"
+bridgePort=60022
+remoteHost='ssh.jakimfett.com'
+remoteUser='bridge'
+
+newBridge() {
+  /usr/bin/ssh ${bridgeFlag} ${bridgePort}:localhost:22 ${remoteUser}@${remoteHost} &
+}
+
+isBridging() {
+	ps aux | grep "${bridgeFlag} ${bridgePort}" | awk '{print $2}'
+}
+
+if [ ! isBridging ]; then
+  echo "no bridge found, creating
+  # newBridge
+fi
