f7420de3d92c8b3a4d951612b3b086735bb2175d
Tessa L. H. Lovelace <jakimfett@assorted.tech>
2019-08-13 10:36:34 -0400

    Adding a much-needed bit of communication paradigm: the emote.

---
+++ com/shrug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# @author:'jakimfett'
+# @license;'cc-by-sa'
+#
+# ¯\_(ツ)_/¯
+
+
+# @shrug.desc
+# Prints an ascii 'shrug' emote.
+#
+# @shrug.syntax
+# 	`shrug.sh` <-- prints the emote
+#
+# @shrug.returns
+#	0
+#
+# @shrug.comment
+#	This is an emote. 
+#	There are many like it, but this one is a shrug.
+#	May not communicate well with species that don't have shoulders.
+function shrug {
+	echo '¯\_(ツ)_/¯'
+	exit 0
+}
+
+shrug
