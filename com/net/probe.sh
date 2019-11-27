a4e8bff1c169f8f4bba217d5e178a4b9019484f5
Tessa L. H. Lovelace <jakimfett@assorted.tech>
2019-11-26 22:43:24 -0800

    Draft, untested. For finding friendlies on the network.

--- com/net/probe.sh
+++ com/net/probe.sh
@@ -3,3 +3,6 @@
 # @license;'cc-by-sa'
 #
 # @todo - scan the network for open ssh, attempt to auth with provided keypair(s)
+
+net=$(ifconfig | grep "inet " | grep -v 127 | awk '{print $2}')
+nmap -n -sn ${net}/24 -oG - | awk '/Up$/{print $2}'
